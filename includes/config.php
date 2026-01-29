5df0891ee75f04c4dd03b07ff796f2aaff5fcf56
Lyani Sitti <33978205+sittilyani@users.noreply.github.com>
2026-01-29 22:41:02 +0300

    updated public and assets

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,12 @@
 <?php
 $host = 'localhost';
-$db = 'butchery_db';
-$user = 'root'; // Default in Laragon/XAMPP
-$pass = ''; // Empty by default
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+$username = 'root';
+$password = '';
+$database = 'methadone';
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
